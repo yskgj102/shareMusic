b45666e6242e351b53033af9dd53b59b1bc22458
ryohei <yskgj102@gmail.com>
2020-06-06 21:31:19 +0900

    Update header.html, index.html, and main.js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,6 @@ jQuery(document).ready(function( $ ) {
 
 
 });
-function header(rootDir){
-    $.ajax({
-        url: rootDir + "include/header.html", //
-        cache: false,
-        async: false,
-        dataType: 'html',
-        success: function(html){
-            html = html.replace(/\{\$root\}/g, rootDir);
-            document.write(html);
-        }
-    });
-}
 
 function navbar(rootDir){
     $.ajax({
@@ -30,7 +18,7 @@ function navbar(rootDir){
 
 function footer(rootDir){
     $.ajax({
-        url: rootDir + "include/footer.html", // 
+        url: rootDir + "include/footer.html", //
         cache: false,
         async: false,
         dataType: 'html',
